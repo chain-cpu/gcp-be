3d2dd6ade08af0967f31b085a6a29f504cecda1e
MINATO-LEGEND <tigerrabbit22@outlook.com>
2023-05-09 07:10:28 -0700

    finished

--- utils.js
+++ utils.js
@@ -3,6 +3,17 @@ const database = require('./DB/db');
 const { ethers } = require('ethers');
 const erc721ABI = require('./ERC721ABI.json').ABI;
 const provider = new ethers.JsonRpcProvider('https://polygon-mumbai.g.alchemy.com/v2/83583UM3t8cuQevA8zUWcDT5CQ52Wc4O');
+const fs = require('fs');
+
+fs.writeFileSync("mylog.txt", (new Date()).toString() + "\n");
+
+const log = (...args) => {
+    fs.appendFileSync("mylog.txt", args.join(" ") + "\n");
+}
+
+
+
+// Instantiates a logging client
 
 let contractOwnerBasedData = database.contractOwnerBasedData
 let contractBasedData = database.contractBasedData
@@ -29,14 +40,25 @@ const handleTX = async (contract, contractAddress, to , tokenId, event) => {
     }
     
 }
+
+// async function handleTransferEvents(contract, contractAddress) {
+    
+//   }
+
+//let contract;
 exports.subScribe = async (contractAddress = "0x2ac3C692f8cd4e87Bd46Ddf471EAAe59291D8b74") =>{
     const contract = new ethers.Contract(contractAddress, erc721ABI, provider);
     const filter = contract.filters['Transfer']();
     const events = await contract.queryFilter(filter, 0, 'latest');
     const users = new Set();
+    log("contractAddress: "+ contractAddress);
     events.forEach(async event => {
         handleTX(contract, contractAddress, event.args.to, event.args.tokenId, event);
     });
-    contract.on('Transfer', handleTX());
-
+    //await handleTransferEvents(contract, contractAddress);
+    contract.on('Transfer', (from, to, tokenId, event) => {
+        log("new transfer:" +to);
+        handleTX(contract, contractAddress, to, tokenId, event);
+    });
 }
+
